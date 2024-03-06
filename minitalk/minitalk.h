9efba0f210785059c326e874026912ff39865104
raphlou5 <43518491+raphlou5@users.noreply.github.com>
2024-03-06 14:51:46 +0100

    Add files via upload

---
+++ minitalk/minitalk.h
@@ -0,0 +1,10 @@
+#ifndef MINITALK_H
+#define MINITALK_H
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <signal.h>
+#include <string.h>
+
+#endif
---
+++ minitalk/server.c
@@ -0,0 +1,50 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   server.c                                           :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: elevast <elevast@student.42.fr>            +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/03/06 13:17:28 by elevast           #+#    #+#             */
+/*   Updated: 2024/03/06 14:26:06 by elevast          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "minitalk.h"
+
+void	ft_btoa(int sig)
+{
+	static int	bit;
+	static int	i;
+
+	if (sig == SIGUSR1)
+		i |= (0x01 << bit);
+	bit++;
+	if (bit == 8)
+	{
+		printf("%c", i);
+		bit = 0;
+		i = 0;
+	}
+}
+
+int	main(int argc, char **argv)
+{
+	int	pid;
+
+	(void)argv;
+	if (argc != 1)
+	{
+		printf("Error\n");
+		return (1);
+	}
+	pid = getpid();
+	printf("%d\n", pid);
+	while (argc == 1)
+	{
+		signal(SIGUSR1, ft_btoa);
+		signal(SIGUSR2, ft_btoa);
+		pause ();
+	}
+	return (0);
+}
